de4d34f015502f710158eb5873f2e59c6bef2d67
samina-shameem <samina.shameem.clt@gmail.com>
2024-09-03 23:26:13 +0200

    Created routing to control the pages

--- chat_box_project/src/App.jsx
+++ chat_box_project/src/App.jsx
@@ -1,43 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { AuthProvider } from './components/context/AuthContext';
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import SideNav from './components/layout/SideNav';
+import Layout from './components/layout/Layout';
 import Home from './components/pages/Home';
 import NotFoundPage from './components/pages/NotFoundPage';
 import RegisterPage from './components/pages/RegisterPage';
-
 import LoginPage from './components/pages/LoginPage';
+import Unauthorized from './components/pages/Unauthorized';
+
+import RequireAuth from './components/auth/RequireAuth';
+import ChatPage from './components/pages/ChatPage';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Header />
-        <SideNav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          {/* <Route path="/chat" element={
-            <PrivateRoute>
-              <ChatPage />
-            </PrivateRoute>
-          } />
-          <Route path="/profile" element={
-            <PrivateRoute>
-              <ProfilePage />
-            </PrivateRoute>
-          } /> */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />        
+
+        {/* we want to protect these routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="/chat" element={<ChatPage />} />        
+        </Route>
+
+        {/* catch all */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 }
 

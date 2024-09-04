8b4ca35217ee062eb484795bdbe7437dedc010c3
samina-shameem <samina.shameem.clt@gmail.com>
2024-09-05 01:51:56 +0200

    Login successful

--- chat_box_project/src/components/hooks/useAxiosPrivate.jsx
+++ chat_box_project/src/components/hooks/useAxiosPrivate.jsx
@@ -1,70 +1,74 @@
-import { useEffect, useState } from "react";
-import { axiosPrivate } from "../api/axios";
-import useAuth from "./useAuth";
+import { useEffect, useState } from 'react';
+import { axiosPrivate } from '../api/axios';
+import useAuth from './useAuth';
+
+const fetchCsrfToken = async (axiosInstance) => {
+  try {
+    const response = await axiosInstance.patch('/csrf');
+    console.debug('Fetched CSRF token');
+    return response.data.csrfToken; // Assuming response contains csrfToken
+  } catch (error) {
+    console.error('Error fetching CSRF token:', error);
+    throw error;
+  }
+};
+
+const generateToken = async (axiosInstance, auth) => {
+  if (!auth?.username || !auth?.password) {
+    throw new Error('No username or password found for token generation');
+  }
+
+  const loginData = { username: auth.username, password: auth.password };
+  try {
+    const response = await axiosInstance.post('/auth/token', loginData);
+    const { token } = response.data;
+    console.info('Generated new token');
+    return token;
+  } catch (error) {
+    console.error('Error refreshing auth token:', error);
+    throw error;
+  }
+};
 
 const useAxiosPrivate = () => {
   const { auth } = useAuth();
   const [csrfToken, setCsrfToken] = useState(null);
 
   // List of endpoints that require CSRF token
-  const csrfEndpoints = ["/auth/register", "/auth/token"];
+  const csrfEndpoints = ['/auth/register', '/auth/token'];
 
   // List of endpoints that require JWT
-  const jwtEndpoints = ["/messages", "/users", "/user"];
-
-  // Function to fetch CSRF token
-  const fetchCsrfToken = async () => {
-    try {
-      const response = await axiosPrivate.patch('/csrf');
-      console.debug('Fetched CSRF token');
-      return response.data.csrfToken; // Assuming response contains csrfToken
-    } catch (error) {
-      console.error('Error fetching CSRF token:', error);
-      throw error;
-    }
-  };
-
-  // Function to generate a new token (for refreshing expired tokens)
-  const generateToken = async () => {
-    if (!auth?.username || !auth?.password) {
-      throw new Error("No username or password found for token generation");
-    }
-
-    const loginData = { username: auth.username, password: auth.password };
-    try {
-      const response = await axiosPrivate.post('/auth/token', loginData);
-      // Assuming the response contains a new token
-      const { token } = response.data;
-      console.info('Generated new token');
-      return token;
-    } catch (error) {
-      console.error('Error refreshing auth token:', error);
-      throw error;
-    }
-  };
+  const jwtEndpoints = ['/messages', '/users', '/user'];
 
   useEffect(() => {
     const requestIntercept = axiosPrivate.interceptors.request.use(
       async (config) => {
+        console.info('request intercepted');
+        console.info(config.url);
         // Add JWT to the Authorization header if required
         if (jwtEndpoints.some((endpoint) => config.url.includes(endpoint))) {
-          if (auth?.accessToken && !config.headers["Authorization"]) {
-            config.headers["Authorization"] = `Bearer ${auth.accessToken}`;
+            console.info("JWT endpoint found");
+          if (auth?.accessToken && !config.headers['Authorization']) {
+            config.headers['Authorization'] = `Bearer ${auth.accessToken}`;
           }
         }
 
         // Add CSRF token to the request body if required
         if (csrfEndpoints.some((endpoint) => config.url.includes(endpoint))) {
+            console.info("CSRF endpoint found");
           if (!csrfToken) {
+            console.info("No csrf token found");
             try {
-              const token = await fetchCsrfToken();
+              const token = await fetchCsrfToken(axiosPrivate);
+              console.info("Fetched csrf token");
+              console.info(token);
               setCsrfToken(token);
               config.data = {
                 ...config.data,
                 csrfToken: token,
               };
             } catch (error) {
-              console.error("Failed to fetch CSRF token:", error);
+              console.error('Failed to fetch CSRF token:', error);
               return Promise.reject(error);
             }
           } else {
@@ -74,7 +78,7 @@ const useAxiosPrivate = () => {
             };
           }
         }
-
+        console.info('request intercepted finished');
         return config;
       },
       (error) => Promise.reject(error)
@@ -86,23 +90,31 @@ const useAxiosPrivate = () => {
         const prevRequest = error?.config;
 
         // Check for 403 Forbidden errors and handle JWT refresh
-        if (error?.response?.status === 403 && !prevRequest?.sent && jwtEndpoints.some((endpoint) => prevRequest.url.includes(endpoint))) {
+        if (
+          error?.response?.status === 403 &&
+          !prevRequest?.sent &&
+          jwtEndpoints.some((endpoint) => prevRequest.url.includes(endpoint))
+        ) {
           prevRequest.sent = true;
           try {
-            const newAccessToken = await generateToken();
-            prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+            const newAccessToken = await generateToken(axiosPrivate, auth);
+            prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
             return axiosPrivate(prevRequest);
           } catch (tokenError) {
-            console.error("Failed to refresh token:", tokenError);
+            console.error('Failed to refresh token:', tokenError);
             return Promise.reject(tokenError);
           }
         }
 
         // Check for 401 Unauthorized errors and handle CSRF token refresh
-        if (error?.response?.status === 401 && !prevRequest?.sent && csrfEndpoints.some((endpoint) => prevRequest.url.includes(endpoint))) {
+        if (
+          error?.response?.status === 401 &&
+          !prevRequest?.sent &&
+          csrfEndpoints.some((endpoint) => prevRequest.url.includes(endpoint))
+        ) {
           prevRequest.sent = true;
           try {
-            const newCsrfToken = await fetchCsrfToken();
+            const newCsrfToken = await fetchCsrfToken(axiosPrivate);
             setCsrfToken(newCsrfToken);
             prevRequest.data = {
               ...prevRequest.data,
@@ -110,7 +122,7 @@ const useAxiosPrivate = () => {
             };
             return axiosPrivate(prevRequest);
           } catch (csrfError) {
-            console.error("Failed to refresh CSRF token:", csrfError);
+            console.error('Failed to refresh CSRF token:', csrfError);
             return Promise.reject(csrfError);
           }
         }

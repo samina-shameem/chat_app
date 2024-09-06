597c776d85e7f35166077abde26e000de88d2867
samina-shameem <samina.shameem.clt@gmail.com>
2024-09-06 18:45:27 +0200

    Fixed registration error

--- chat_box_project/src/components/pages/LoginPage.jsx
+++ chat_box_project/src/components/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
